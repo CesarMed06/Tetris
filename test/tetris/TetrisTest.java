4e581c9a0620929fd7e6899bce7fd4e6f9548159
CesarMed06 <cmedsag@alumnos.cei.es>
2025-03-14 11:27:20 +0100

    finished

--- test/tetris/TetrisTest.java
+++ test/tetris/TetrisTest.java
@@ -20,5 +20,17 @@ class TetrisTest {
 	void testInitialScoreIsZero() {
 			assertEquals(0, game.currentScore(), "The score doesn't initialize as zero");
 		}
-}
+	
+	@Test
+	void testPieceCanMoveDown() {
+		Tetromino piece = new Tetromino(new Point[] { new Point(4,				
+		0), new Point(5,0) }, Color.RED);
+	}
 
+	@Test
+	void testPieceCannotMoveOutsideLeft() {
+		for (int x = 0; x < 10; x++) {
+				board.grid[19][x] = Color.GREEN;
+		}
+	}
+	}
